caeeae7241c35bdf5ec10dd8d1b2750def71a92f
Lloyd Johnson <lloyd@theljohnson.com>
2016-11-12 15:39:28 -0800

    Welp i found a slide showing the hover read bio on the team members, so i had to go back and add that too

---
+++ js/script.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+    
+
+	$('#team-members a.aligncenter').hover(
+		
+	function() {
+	    $(this).append('<div class="bio-text"><span>Read Bio</span></div><div class="clearfix"></div>')
+	        $('.both').animate({opacity: 1.0}) 
+	}, 
+	function(){
+        $('#team-members a.aligncenter div.bio-text').fadeOut(1, function(){
+            $(this).remove()
+        })
+	});
+
+
+});
